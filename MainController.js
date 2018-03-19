483cc3eb69e6b4ec5a90f2754ce88d55e771b725
Connor Sharp <cjsharp99@gmail.com>
2018-03-19 10:56:47 -0500

    Fixes to img and button

--- MainController.js
+++ MainController.js
@@ -1,6 +1,7 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.likes = 0;
     $scope.plusOne = function() {
+        console.log('Test function');
         $scope.likes ++;
     }
 }]);
